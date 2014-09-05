f5a592f74ed5adc50fd6ed2bb6cea58fdbeda0cc
Andy Turner <a.turner@epcc.ed.ac.uk>
2014-09-05 12:33:27 +0100

    Added MOM node monitoring

---
+++ monitoring/mom-nodes/cpu_and_mem_use.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get current date/time
+TIME=`date --rfc-3339=seconds`
+
+# Get the appropriate lines from uptime
+uplines=`uptime`
+# Split the lines into an array for processing
+uptokens=($uplines)
+
+# Get the appropriate lines from free
+freelines=`free -g | awk '(NR>1)&&(NR<4)'`
+# Split the lines into an array for processing
+freetokens=($freelines)
+
+# Print data:
+#          CPU                                             Memory
+#          User         System       Idle                  Total            Used             Free       Cache
+echo $TIME ${uptokens[11]} ${freetokens[1]} ${freetokens[9]} ${freetokens[10]} ${freetokens[6]}
