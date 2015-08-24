b0d54d135e0e8a80ddaf01e57e8a6a04254af6d3
Andy Turner <a.turner@epcc.ed.ac.uk>
2015-08-24 14:39:18 +0100

    Updated RAID check to print status to web

--- monitoring/raid/get_raid.bash
+++ monitoring/raid/get_raid.bash
@@ -20,9 +20,33 @@ raid_status=`$RAID_CHECK`
 # Write the start indicator and timestamp 
 TIME=`date --rfc-3339=seconds`
 DATE=`date --rfc-3339=date`
+NICEDATE=`date +"%H:%M, %A %d %B %Y"`
 
 outfile="$ARCHER_MON_LOGDIR/raid/$DATE.raid"
 
 echo $TIME $raid_status >> $outfile
+
+# Write HTML fragment for status webpage
+fslist="fs2 fs3 fs4"
+htmlfile="$ARCHER_MON_OUTDIR/usage/raid.html"
+echo "" > $htmlfile
+for fs in $fslist; do
+   raid_stat=`raidStatus $fs`
+   if [ $raid_stat == "1" ]; then
+cat >> $htmlfile <<EOF
+<div id="machine_status_up">
+   <p>RAID check, ${fs}: <span style="color: green">Running</span></p>
+   <p>Last Updated: $NICEDATE</p>
+</div>
+EOF
+else
+cat >> $htmlfile <<EOF
+<div id="machine_status_down">
+   <p>RAID check, ${fs}: <span style="color: red">Not running</span></p>
+   <p>Last Updated: $NICEDATE</p>
+</div>
+EOF
+fi
+done
 
 exit 0
